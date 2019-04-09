3c9791ef70a045412d5038418d15a3af6cd86a19
eudorcas <klaudia.butkiewicz09@gmail.com>
2019-04-09 23:53:47 +0200

    Task done

---
+++ src/index.js
@@ -0,0 +1,74 @@
+import 'babel-polyfill';
+
+function* secondsGenerator() {
+    let i = yield;
+    yield;
+    while (true) {
+        yield ++i;
+        if (i >= 59) {
+            yield i = 0;
+        }
+    }
+}
+
+function* minutesGenerator() {
+    let j = yield;
+    yield;
+    while (true) {
+        yield ++j;
+        if (j >= 59) {
+            yield j = 0;
+        }
+    }
+}
+
+function* hoursGenerator() {
+    let z = yield;
+    yield;
+    while (true) {
+        yield ++z;
+        if (z >= 12) {
+            yield z = 0;
+        }
+    }
+}
+
+const addDigit = (number) => {
+    return number < 10 ? "0"+number : number
+}
+const date = new Date();
+const currentH = Math.floor(date.getHours()/2);
+const currentM = date.getMinutes();
+const currentS = date.getSeconds();
+
+document.getElementById("hours").innerHTML = addDigit(currentH);
+document.getElementById("minutes").innerHTML = addDigit(currentM);
+document.getElementById("seconds").innerHTML = addDigit(currentS);
+
+const seconds = secondsGenerator();
+const minutes = minutesGenerator();
+const hours = hoursGenerator();
+
+seconds.next();
+minutes.next();
+hours.next();
+
+seconds.next(currentS);
+minutes.next(currentM);
+hours.next(currentH);
+
+setInterval(() => {
+    const sec = seconds.next().value;
+    document.getElementById("seconds").innerHTML = addDigit(sec);
+
+    if (sec === 59) {
+        const min = minutes.next().value;
+        document.getElementById("minutes").innerHTML = addDigit(min);
+
+        if (min === 59) {
+            const h = hours.next().value;
+            document.getElementById("hours").innerHTML = addDigit(h);
+        }
+    }
+}, 1000);
+
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
